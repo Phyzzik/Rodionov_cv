8566f9f3bff986dc2186040020bf8a9363afb01c
Phyzzik <bqgk62159@gmail.com>
2026-03-20 03:07:30 +0800

    Добавить файл task.py

---
+++ Numpy_task/task.py
@@ -0,0 +1,64 @@
+#Задание 1
+
+# slovo = input("Введи 1 число, операцию (-, +, *, /), 2 число : ")
+
+# razbit = slovo.split()
+
+# a = razbit[0]
+# znak = razbit[1]
+# b = razbit[2]
+
+# a = int(a)
+# b = int(b)
+
+# if znak == "-":
+#     print(a - b)
+# elif znak == "+":
+#     print (a + b)
+# elif znak == "*":
+#     print (a * b)
+# elif znak == "/":
+#     print (a / b)
+# else:
+#     print("Такого нету введи из предложенных")
+
+
+
+#Задание для закрепления 
+#
+#rastoyanie = input("Введи число, единицу измерения (м,км,см), и в какую хочешь перевести: ")
+#
+#raz = rastoyanie.split()
+#
+#a = raz[0]
+#b = raz[1]
+#c = raz[2]
+#
+#a = int(a)
+#    
+#if b == "м" and c == "см":
+#    print(a * 100)
+#elif b == "км" and c == "м":
+#    print(a * 1000)
+#elif b == "см" and c == "км":
+#    print(a / 100000)
+#elif b == "м" and c == "км":
+#    print(a / 1000)
+#elif b == "км" and с == "см":
+#    print(a * 100000)
+#elif b == "см" and c == "м":
+#    print(a / 100)
+#elif b == "см" and c == "cм":
+#    print(a)
+#elif b == "м" and c == "м":
+#    print(a)
+#elif b == "км" and c == "км":
+#    print(a)
+#else: 
+#    print("Ну так не бывает, выбери что то проще (я показал)")
+
+papka = open('world.txt', 'r')
+
+
+
+
